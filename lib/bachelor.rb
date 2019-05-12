4f74eedffaefa3e77ecd6ffed56e1c2c37068d39
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-05-12 20:30:02 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -66,7 +66,7 @@ def get_average_age_for_season(data, season)
         end
       end
     end
-  puts ages = (ages/counter).round
+  puts ages = (ages/counter).float
   puts ages 
   #puts ages.round
 end
